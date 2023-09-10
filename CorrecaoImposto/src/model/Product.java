51b21849219be1170e3caf7e063205c9148def78
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-09-10 15:32:58 -0300

    Add files via upload

--- CorrecaoImposto/src/model/Product.java
+++ CorrecaoImposto/src/model/Product.java
@@ -7,13 +7,16 @@ public class Product {
 	private String name;
 	private double costPrice;
 	private double profit;
+    private boolean industrialized = false;
+    private String imported;
 	private List<Tax> taxes;
 	
-	public Product(String name, double costPrice, double profit) throws Exception{
+	public Product(String name, double costPrice, double profit, String imported) throws Exception{
 		setName(name);
 		setCostPrice(costPrice);
 		setProfit(profit);
-		this.taxes = new ArrayList<Tax>();
+	    this.imported = imported;
+	    this.taxes = new ArrayList<Tax>();
 	}
 	
 	public boolean addTax(Tax tax) throws Exception{
@@ -21,10 +24,16 @@ public class Product {
 		if(tax == null)
 			throw new Exception("Imposto não pode ser nulo");
 		
-		if(taxes.contains(tax))
-			return false;
-		else taxes.add(tax);
+		if(taxes.contains(tax)) {
+			return false;			
+		}
 		
+		else{
+			if(tax.getAbbr().equals("IPI"))
+				industrialized = true;
+			
+			taxes.add(tax);
+		}	
 		return true;
 	}
 	
@@ -73,5 +82,17 @@ public class Product {
 	
 	public String getName() {
 		return  this.name;
+	}
+	
+	public double getProfit() {
+		return this.profit;
+	}
+	
+	public String getImported() {
+		return imported;
+	}
+
+	public boolean getIndustrialized() {
+		return industrialized;
 	}
 }
--- CorrecaoImposto/src/view/Main.java
+++ CorrecaoImposto/src/view/Main.java
@@ -5,15 +5,20 @@ import model.Tax;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		Product arroz = new Product("arroz",100,30);
-		
-		Tax IPI = new Tax("IPI", "Imposto Prod. Industrializado", 8);
-		Tax ISS = new Tax("ISS", "ISS", 12);
+    public static void main(String[] args) throws Exception {
+       
+        Product arroz = new Product("arroz", 7, 20, "nacional");
 
-		arroz.addTax(IPI);
-		arroz.addTax(ISS);
-		
-		System.out.println(arroz.getName()+ " R$"+arroz.calculateSalePrice());
-	}
+        Tax IPI = new Tax("IPI", "Imposto Prod. Industrializado", 8);
+        Tax ISS = new Tax("ISS", "ISS", 5);
+        Tax ICMS = new Tax("ICMS", "ICMS", 5);
+
+        arroz.addTax(ICMS);
+        arroz.addTax(ISS);
+
+        double totalPrice = arroz.calculateSalePrice();
+
+        System.out.println(arroz.getName() + " - Preço de venda com impostos:");
+        System.out.printf("Preço Total: R$%.2f\n",totalPrice);
+    }
 }
